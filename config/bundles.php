aa9a5fd5e5ca17a697552344a933b2b2ba4fe02c
vbernath_axistwelve <viktor.bernath@axistwelve.com>
2022-11-07 22:23:03 +0100

    Missed updates

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,5 @@ return [
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Liip\TestFixturesBundle\LiipTestFixturesBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
